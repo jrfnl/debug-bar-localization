af990d4150cc5bb41d01e0327a03f9cd57d0264e
jrfnl <github_nospam@adviesenzo.nl>
2016-04-12 04:46:18 +0200

    Make loading of text-domain compatible with use of the plugin in the `must-use` plugins directory.

--- class-debug-bar-localization.php
+++ class-debug-bar-localization.php
@@ -71,8 +71,7 @@ if ( ! class_exists( 'Debug_Bar_Localization' ) && class_exists( 'Debug_Bar_Pane
 		 * Constructor.
 		 */
 		public function init() {
-			load_plugin_textdomain( 'debug-bar-localization', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
-
+			$this->load_textdomain( self::NAME );
 			$this->title( __( 'Localization', 'debug-bar-localization' ) );
 
 			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
@@ -90,6 +89,27 @@ if ( ! class_exists( 'Debug_Bar_Localization' ) && class_exists( 'Debug_Bar_Pane
 				'plugin'   => __( 'Plugins', 'debug-bar-localization' ),
 				'unknown'  => __( 'Unknown', 'debug-bar-localization' ),
 			);
+		}
+
+
+		/**
+		 * Load the plugin text strings.
+		 *
+		 * Compatible with use of the plugin in the must-use plugins directory.
+		 *
+		 * @param string $domain Text domain to load.
+		 */
+		protected function load_textdomain( $domain ) {
+			if ( is_textdomain_loaded( $domain ) ) {
+				return;
+			}
+
+			$lang_path = dirname( plugin_basename( __FILE__ ) ) . '/languages';
+			if ( false === strpos( __FILE__, basename( WPMU_PLUGIN_DIR ) ) ) {
+				load_plugin_textdomain( $domain, false, $lang_path );
+			} else {
+				load_muplugin_textdomain( $domain, $lang_path );
+			}
 		}
 
 
